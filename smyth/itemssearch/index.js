0a21e3436fa5f2842eaca743fdd8af60ee15eca0
zandyisrad <34150410+zandyisrad@users.noreply.github.com>
2018-07-04 11:00:10 -0700

    Update index.js

--- smyth/itemssearch/index.js
+++ smyth/itemssearch/index.js
@@ -1,5 +1,12 @@
+Item = function(name,desc,rarity,cost){
+  this.name = name;
+  this.desc = desc;
+  this.rarity = rarity;
+  this.cost = cost;
+};
+
 var raad = new Object();
 
-raad["Fire&Sword"] =  "Flaming Sword";
-raad["Metal&Stick"] = "Sword";
-raad["Captain&Rad&Tuna"] = "Captain Rad Tuna";
+raad["Fire&Sword"] =  new Item("Flaming Sword","It's a sword thats on fire!","Rarerer","10 gp");
+raad["Metal&Stick"] = new Item("Sword","It's a sword.","Common","1 cp");
+raad["Captain&Rad&Tuna"] = new Item("Captain Rad Tuna","Captain Rad Tuna.","Legendary","Priceless");
